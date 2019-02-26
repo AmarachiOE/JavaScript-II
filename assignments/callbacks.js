db545635baf2258d7c6de39541111dbf99ead9e9
AmarachiOE <ao.ejiawoko@gmail.com>
2019-02-26 12:17:10 -0800

    completed callbacks.js problems (not stretch)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,27 +24,66 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// PROBLEM 1
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+getLength(items, function(length) {
+  console.log(length);
+});
+
+// PROBLEM 2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
+
+// PROBLEM 3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+sumNums(2, 4, function(add) {
+  console.log(add);
+});
+
+// PROBLEM 4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+multiplyNums(3, 5, function(mult) {
+  console.log(mult);
+});
+
+// PROBLEM 5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+  return cb(console.log("True!"));
+  }
+
+  else {
+    return cb(console.log("False!"))
+  }  
 }
+
+contains('yo-yo', items, function(call){
+  console.log(call);
+});
+
+contains('barbie', items, function(call){
+  console.log(call);
+});
 
 /* STRETCH PROBLEM */
 
